214989daf53ce8027fa274ef7389e44b50d7116a
renato29 <some@email.com>
2020-11-26 18:49:28 +0200

    teste

--- magic8ball/script.js
+++ magic8ball/script.js
@@ -1,5 +1,25 @@
+
+
 // fazer interacao do botao pelo DOM pegar o botao e o campo de resposta
-let btn =  document.querySelector('button')
+let btn = document.querySelector('button')
 let awnser = document.querySelector('balltalked')
 
+function rando_num(max_number) {
+    return Math.round(Math.random() * max_number)
+}
 //agora queremos adicionar um evento de click do usuario.
+button.addEventListener("click", function () {
+    //gerar um numero aleatorio 
+
+
+    //criar um numero
+    let random_number = rando_num(max_number);
+    alert(random_number);
+
+
+    //transformar o numero sorteado em texto 
+
+
+    //mostrar a resposta no campo dedicado.
+
+})
