1b3b65a7b442ff6d6b58f871897f8a66599653d2
renato29 <some@email.com>
2020-11-26 16:57:09 +0200

    parcial commit

---
+++ magic8ball/script.js
@@ -0,0 +1,5 @@
+// fazer interacao do botao pelo DOM pegar o botao e o campo de resposta
+let btn =  document.querySelector('button')
+let awnser = document.querySelector('balltalked')
+
+//agora queremos adicionar um evento de click do usuario.
