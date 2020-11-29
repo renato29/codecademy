361a236ac1ba7405a2c61c0ccf38aa96c1c4b83e
renato29 <some@email.com>
2020-11-29 11:01:19 +0200

    final_commit

--- magic8ball/script.js
+++ magic8ball/script.js
@@ -1,25 +1,34 @@
 
-
+function rando_num(max_number) {
+    return Math.round(Math.random() * max_number);
+}
 // fazer interacao do botao pelo DOM pegar o botao e o campo de resposta
 let btn = document.querySelector('button')
-let awnser = document.querySelector('balltalked')
+let balltalked = document.querySelector('balltalked')
 
-function rando_num(max_number) {
-    return Math.round(Math.random() * max_number)
-}
+
 //agora queremos adicionar um evento de click do usuario.
-button.addEventListener("click", function () {
+button.addEventListener("click", function(){
     //gerar um numero aleatorio 
+    //criar um numero da resposta
+    let msg = rando_num(3);
+    // alert(numero); mostra o numero sorteado
+    //transformar o numero sorteado em texto,ciar a variavel vazia aonde entra a string
+    let msgtext= "";
 
-
-    //criar um numero
-    let random_number = rando_num(max_number);
-    alert(random_number);
-
-
-    //transformar o numero sorteado em texto 
-
-
+    if(msg==0){
+        msgtext = " My reply is no.";
+    }
+    else if (msg== 1 ){
+        msgtext = " Signs point to yes.";
+    }
+    else if (msg== 2){
+        msgtext = "Yes ";
+    }
+    else{
+        msgtext = "Don’t count on it.";
+    }
+    balltalked.innerText = msgtext;
     //mostrar a resposta no campo dedicado.
 
-})
+});
